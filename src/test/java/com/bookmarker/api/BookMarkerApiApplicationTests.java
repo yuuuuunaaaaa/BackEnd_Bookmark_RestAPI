3281c1501c311635c7b25c8b23940b25fa828707
backYuna <9354yuna@gmail.com>
2024-05-20 10:56:15 +0900

    initial commit

---
+++ src/test/java/com/bookmarker/api/BookMarkerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmarker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarkerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
